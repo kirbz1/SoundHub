e4a8fc45d03a8cdffa45d6e98135ed7c5668c933
Kirby Smith <ksmi4344@uni.sydney.edu.au>
2024-02-18 19:07:43 +1100

    refresh

--- soundhub_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SongListCreateView, SongRetrieveUpdateDeleteView
-
-urlpatterns = [
-    path('api/songs/', SongListCreateView.as_view(), name='song-list-create'),
-    path('api/songs/<int:pk>/', SongRetrieveUpdateDeleteView.as_view(), name='song-retrieve-update-delete'),
-]
