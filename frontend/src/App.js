7405b701cdd937cf6cbf864d6ee863b937bf620b
Kirby Smith <ksmi4344@uni.sydney.edu.au>
2024-02-19 23:09:14 +1100

    frontend login/register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import NotFound from './components/NotFound';
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Navbar, Container, Button } from 'react-bootstrap';
-
-function App() {
-
-  const [songs, setSongs] = useState([])
-
-  useEffect(() => {
-    fetch("/songs")
-    .then(response => response.json())
-    .then(data => {setSongs(data)})
-  }, [])
-
-
-  return(    
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>SoundHub</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              <a href="http://localhost:3000/login">
-                <Button variant="light">Login</Button>
-              </a>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <div className='center'><h2>Top songs</h2></div>
-
-      <div className='center'>
-        <ul>
-          {(typeof songs === 'undefined') ? (
-            <p>Loading songs...</p>
-            ): (
-              songs.map(song => (
-                <li key={song.id}>{song.title} by {song.artist}</li>
-              ))
-            )}
-        </ul>
-        
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/register" element={<Register/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
       </div>
-    </div>
-  )
+    </Router>
+  );
 }
 
 export default App;
