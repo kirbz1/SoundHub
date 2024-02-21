bb9fba819af25b12b797f6b60794a576e4cc5ade
Kirby Smith <ksmi4344@uni.sydney.edu.au>
2024-02-21 17:19:03 +1100

    ui elements planning future functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import Profile from './components/Profile';
 import NotFound from './components/NotFound';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,6 +16,7 @@ const App = () => {
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/register" element={<Register/>} />
+          <Route exact path="/profile" element={<Profile/>} />
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </div>
